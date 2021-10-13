7f5b55d3a9291724bacfe51ff7b2232e8325512a
Cristian Mendes <cristianpmendes@gmail.com>
2021-10-13 11:36:50 -0300

    Criando uma API

---
+++ FilmesAPI/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FilmesAPI.Models;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class FilmeController : ControllerBase
+    {
+        public static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        public void AdicionaFilme([FromBody] Filme filme)
+        {
+            filmes.Add(filme);
+            Console.WriteLine(filme.Titulo);
+        }
+    }
+}
---
+++ FilmesAPI/FilmesAPI/Models/Filme.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage = "É necessário inserir o titulo!")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "É necessário inserir o diretor!")]
+        public string Diretor { get; set; }
+        public string Genero { get; set; }
+        [Range(1,300, ErrorMessage = "Duração invalida.")]
+        public int Duracao { get; set; }
+    }
+}
